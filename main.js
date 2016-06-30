5b950fbff6d505f203395ac59c12c8027fe9746c
Brute79 <andreas@nygaard.as>
2016-06-30 19:35:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Kathrine Beathe Holm'; 
+
+var myHeading = document.querySelector('h3');
+myHeading.textContent = 'Sammen i sentrum'
